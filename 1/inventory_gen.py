1191ba0569878a6cb9a4c50064b7fa4d4f9de1c1
struc2ture <struc0ture@gmail.com>
2025-08-20 12:55:33 -0700

    1

--- 1/inventory_gen.py
+++ 1/inventory_gen.py
@@ -1,5 +1,21 @@
+import random
+
+ITEMS = [
+    'Piece of a broken clay pot',
+    'Half-eaten googlut',
+    'Quill',
+    'Broken-off sword hilt"',
+    'Scrap paper',
+    'Inkpot with dried ink',
+    'Round stone',
+    'Slingshot',
+    'Sling',
+    'Piece of old-world circuitry'
+]
+
 def main():
-    print("hello world")
+    inventory = random.sample(ITEMS, 3)
+    print(inventory)
 
 if __name__ == "__main__":
     main()
